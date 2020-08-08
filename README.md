b5defb509aa5d82538e3396c45044505484f5cfe
d0gg3r <mr.doge2019@gmail.com>
2020-08-08 20:30:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A very simple python script to bulk resize images to desired size.
+
+# Usage
+[Install the required dependencies.](DEPENDENCIES.md)
+Make sure the script is in a seperate folder *along with the images you want resized.*
+Once ran you'll be asked to input your desired width, height and whether to preserve the aspect ratio of the images.
+This script supports [most common image formats](https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#fully-supported-formats), any other files will be ignored.
---
+++ main.py
@@ -0,0 +1,72 @@
+from PIL import Image
+from sys import platform
+from os import system, listdir
+from os.path import isfile
+
+class BoolValueError(Exception):
+  """Raised when input is not 'Y' or 'N'"""
+  pass
+class TooLargeError(Exception):
+  """Raised when the size specified is out of bounds"""
+  pass
+class NoImagesError(Exception):
+  """Raided when there are no images in the directory"""
+  pass
+
+def Error(message):
+  print("")
+  print("\033[31m"+message+"\033[0m")
+def BoolInput(message):
+  response = input(message)
+  if response.lower() != "yes" and response.lower() != "no":
+    raise BoolValueError
+    return
+  boolean = response.lower() == "yes"
+  return boolean
+def clear():
+  print(chr(27)+'[2j')
+  print('\033c')
+  print('\x1bc')
+
+try:
+  if platform.lower() == "win32":
+    system('color')
+  clear()
+  
+  ResizeTo = (int(input("What width should I scale the image to: ")),int(input("What height should I scale the image to: ")))
+  PreserveRatio = BoolInput("Preserve aspect ratio during resize? (Yes/No) ")
+  clear()
+  print("Resizing...")
+  
+  filesAmount = 0
+  for file in list(filter(lambda f: isfile(f), listdir())):
+  
+    try:
+      image = Image.open(file)
+    except IOError:
+      continue
+    
+    if PreserveRatio:
+      image.thumbnail(ResizeTo, Image.NEAREST)
+    else:
+      image = image.resize(ResizeTo)
+    
+    filenameArray = file.split(".")
+    image.save("{0}-{1}x{2}.{3}".format(".".join(filenameArray[0:len(filenameArray)-1]), image.width, image.height, filenameArray[len(filenameArray)-1]))
+    filesAmount+=1
+  if (filesAmount == 0): raise NoImagesError
+ 
+  
+except IOError:
+  Error("Something went wrong when trying to load an image. Double-check for any non-image files in the current directory and try again.")
+except ValueError:
+  Error("The supplied value needs to be a number - don't include the 'px'!")
+except BoolValueError:
+  Error("The supplied value may be 'Y' or 'N' only.")
+except NoImagesError:
+  Error("There are no applicable images in this directory! Check where you're running this from and try again.")
+except:
+  Error("An unexpected error occured.")
+else:
+  print("Finished successfuly!")
+  print("All files were saved by their original name with the new size suffixed.")
